00c1206fcf5a66bcee51a2ad86f12c68c6634818
HammadJameel222 <hammadjameel222@gmail.com>
2025-12-03 01:48:39 +0500

    EQWEWQE

--- core/Router.php
+++ core/Router.php
@@ -35,13 +35,10 @@ class Router
 
         if ($callback === false) {
             $this->response->setStatusCode(404);
-            exit;
+            return $this->renderContent("Not Found");
+           
         }
         if (is_string($callback)) {
-            // Assuming the callback is a view name, render the view
-            // ob_start();
-            // include_once __DIR__ . '/../views/' . $callback . '.php';
-            // return ob_get_clean();
             return $this->renderView($callback);
         }
         return call_user_func($callback);
@@ -52,15 +49,18 @@ class Router
         $layout = $this->layoutView();
         $viewContent = $this->renderOnlyView($view);
         return str_replace('{{ content }}', $viewContent, $layout);
-
+        
         // include_once Application::$ROOT_DIR."/views/$view.php";
+    }
+    
+    function renderContent($viewContent)
+    {
+        $layout = $this->layoutView();
+        return str_replace('{{ content }}', $viewContent, $layout);
     }
 
     protected function layoutView()
     {
-        // ob_start();
-        // include_once __DIR__ . '/../views/layout/main.php';
-        // return ob_get_clean();
         ob_start();
         include_once Application::$ROOT_DIR . "/views/layout/main.php";
         return ob_get_clean();
--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,13 @@
-<h1>Contact Us</h1>
+<form>
+    <div class="mb-3">
+        <label class="form-label">Subject</label>
+        <input type="text" name="subject" class="form-control">
+        
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
